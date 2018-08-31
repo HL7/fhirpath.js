f20f982fc195e2185510c93b1c65285d5fd650ca
niquola <niquola@gmail.com>
2018-09-01 00:56:38 +0300

    inline math

--- src/fhirpath.js
+++ src/fhirpath.js
@@ -23,6 +23,7 @@ let existence = require("./existence");
 let filtering = require("./filtering");
 let misc      = require("./misc");
 let equality  =  require("./equality");
+let math      =  require("./math");
 
 engine.invocationTable = {
   empty:        {fn: existence.emptyFn},
@@ -294,6 +295,65 @@ engine.InequalityExpression = function(ctx, parentData, node) {
   return infixInvoke(ctx, op, parentData, node.children);
 };
 
+engine.AdditiveExpression = function(ctx, parentData, node) {
+  let left = engine.doEval(ctx, parentData, node.children[0]);
+  let right = engine.doEval(ctx, parentData, node.children[1]);
+  util.assertAtMostOne(left, "AdditiveExpression");
+  util.assertAtMostOne(right, "AdditiveExpression");
+  let operator = node.terminalNodeText[0];
+  if (operator === "&") {
+    left = left.length > 0 ? left[0] : "";
+    right = right.length > 0 ? right[0] : "";
+    util.assertType(left, ["string"], "AdditiveExpression");
+    util.assertType(right, ["string"], "AdditiveExpression");
+    return [left + right];
+  }
+  else { // + or -
+    if (left.length === 0 || right.length === 0)
+      return [];
+    else {
+      left = left[0];
+      right = right[0];
+      if (typeof left !== typeof right)
+        throw "AdditiveExpression:  Mismatched types, "+typeof left +" and "+ typeof right;
+      if (operator === "+")
+        return [left + right];
+      else if (operator === "-") {
+        util.assertType(left, ["number"], "AdditiveExpression");
+        util.assertType(right, ["number"], "AdditiveExpression");
+        return [left - right];
+      }
+      else // should never reach here, per the grammar
+        throw "AdditiveExpression: Unexpected operator: " +operator;
+    }
+  }
+};
+
+  engine.MultiplicativeExpression = function(ctx, parentData, node) {
+    let left = engine.doEval(ctx, parentData, node.children[0]);
+    let right = engine.doEval(ctx, parentData, node.children[1]);
+    util.assertAtMostOne(left, "MultiplicativeExpression");
+    util.assertAtMostOne(right, "MultiplicativeExpression");
+    if (left.length == 0 || right.length === 0)
+      return [];
+    left = left[0];
+    right = right[0];
+    util.assertType(left, ["number"], "MultiplicativeExpression");
+    util.assertType(right, ["number"], "MultiplicativeExpression");
+    let operator = node.terminalNodeText[0];
+    if (operator === "*")
+      return [left * right];
+    else if (operator === "/")
+      return [left / right];
+    else if (operator === "mod")
+      return [left % right];
+    else if (operator === "div")
+      return [Math.floor(left / right)];
+    else // should never reach here, per grammar
+      throw "MultiplicativeExpression: Unexpected operator: " +operator;
+  };
+
+
 engine.evalTable = {
   BooleanLiteral: engine.BooleanLiteral,
   EqualityExpression: engine.EqualityExpression,
@@ -303,6 +363,8 @@ engine.evalTable = {
   IndexerExpression: engine.IndexerExpression,
   InequalityExpression: engine.InequalityExpression,
   InvocationExpression: engine.InvocationExpression,
+  AdditiveExpression: engine.AdditiveExpression,
+  MultiplicativeExpression: engine.MultiplicativeExpression,
   InvocationTerm: engine.InvocationTerm,
   LiteralTerm: engine.LiteralTerm,
   MemberInvocation: engine.MemberInvocation,
@@ -314,7 +376,6 @@ engine.evalTable = {
   UnionExpression: engine.UnionExpression,
 };
 
-require("./math")(engine);
 
 engine.doEval = function(ctx, parentData, node) {
   const evaluator = engine.evalTable[node.type];
