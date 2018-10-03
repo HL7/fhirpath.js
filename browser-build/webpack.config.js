5703757eff7ebfab9f8212115209283c3d59a52e
Paul Lynch <plynch@mail.nih.gov>
2018-10-03 17:26:51 -0400

    Added a browser-ready build, and zipped it for use as a GitHub Release file

--- browser-build/webpack.config.js
+++ browser-build/webpack.config.js
@@ -2,10 +2,10 @@ module.exports = {
   node: {
     fs: "empty"
   },
-  entry: './app/scripts/fhirpath/fhirpathRequire.js',
+  entry: './fhirpathRequire.js',
   mode: 'production',
   output: {
     path: __dirname,
-    filename: './app/scripts/fhirpath/fhirpath.min.js',
+    filename: './fhirpath.min.js',
   }
 };
