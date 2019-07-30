343c4bfdc3a930323491e6741c360c4b268a96d3
Joseph Thompson <joseph.thompson@bestpractice.org.nz>
2019-07-30 22:06:35 +0000

    update browserbuild webpack config

--- browser-build/webpack.config.js
+++ browser-build/webpack.config.js
@@ -4,10 +4,24 @@ module.exports = {
   },
   entry: './fhirpathRequire.js',
   mode: 'production',
-//  mode: 'development',
-//devtool: 'source-map',
+  // mode: 'development',
+  // devtool: 'source-map',
   output: {
     path: __dirname,
     filename: './fhirpath.min.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
+      }
+    ]
   }
 };
