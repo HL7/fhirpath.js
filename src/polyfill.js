8d705b95bc3662c731cdf37a74e702b1ed2da1a2
Brian Postlethwaite <brian_pos@hotmail.com>
2022-09-17 16:24:11 -0400

    atob/btoa polyfils as suggested by Yuri (which makes the unit test for encode work with nodejs)

--- src/polyfill.js
+++ src/polyfill.js
@@ -60,3 +60,17 @@ if (!Object.assign) {
     }
   });
 }
+
+// Define btoa for NodeJS
+if (typeof btoa === 'undefined') {
+  global.btoa = function (str) {
+    return new Buffer(str, 'binary').toString('base64');
+  };
+}
+
+// Define atob for NodeJS
+if (typeof atob === 'undefined') {
+  global.atob = function (b64Encoded) {
+    return new Buffer(b64Encoded, 'base64').toString('binary');
+  };
+}
