428d425e5118399bdc95febf18badf731993f4f9
sedinkinya <yury.sedinkin@nih.gov>
2024-01-26 18:01:49 -0500

    Updated benchmark for function descendants()
    LF-2773

--- test/benchmark/descendants.js
+++ test/benchmark/descendants.js
@@ -3,14 +3,14 @@ const _ = require('lodash');
 module.exports = ({
                     current_fhirpath,
                     current_r4_model,
-                    minimumDataset,
+                    patientExample,
                     options
                   }) => {
   // We need copy of objects because the __path__ property is not configurable and may have different values in different versions
-  const minimumDatasetCopy1 = _.cloneDeep(minimumDataset);
-  const minimumDatasetCopy2 = _.cloneDeep(minimumDataset);
-  const numberOfItems = current_fhirpath.evaluate(minimumDatasetCopy1,'Questionnaire.descendants().count()', {},  current_r4_model);
-  const expression = 'Questionnaire.descendants()';
+  const patientExampleCopy1 = _.cloneDeep(patientExample);
+  const PatientExampleCopy2 = _.cloneDeep(patientExample);
+  const numberOfItems = current_fhirpath.evaluate(patientExampleCopy1,'Patient.descendants().count()', {},  current_r4_model);
+  const expression = 'Patient.descendants()';
 
   return [{
     name: `Getting ${numberOfItems} descendants`,
@@ -22,13 +22,13 @@ module.exports = ({
         : [{
           name: `using evaluate()`,
           testFunction: (fhirpath, model) => {
-            fhirpath.evaluate(fhirpath === current_fhirpath ? minimumDatasetCopy1 : minimumDatasetCopy2, expression, {}, model);
+            fhirpath.evaluate(fhirpath === current_fhirpath ? patientExampleCopy1 : PatientExampleCopy2, expression, {}, model);
           }
         }]),
       {
         name: `using compile()`,
         testFunction: (fhirpath, model, compiledFn) => {
-          compiledFn(fhirpath === current_fhirpath ? minimumDatasetCopy1 : minimumDatasetCopy2, {});
+          compiledFn(fhirpath === current_fhirpath ? patientExampleCopy1 : PatientExampleCopy2, {});
         }
       }
     ]
