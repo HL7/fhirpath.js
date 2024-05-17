842367bdd405382004e0eb113e6d605353fe6074
sedinkinya <yury.sedinkin@nih.gov>
2024-05-17 15:44:23 -0400

    Changes as per review
    LF-2100

--- test/supplements.test.js
+++ test/supplements.test.js
@@ -11,6 +11,14 @@ const input = {
     // Clone input file contents to avoid one test affecting another
     return _.cloneDeep(require("../test/resources/phq9-response.json"));
   },
+  get questionnaireResponseWithEmbeddedScores() {
+    // Clone input file contents to avoid one test affecting another
+    return _.cloneDeep(require("./resources/phq9-response-with-embedded-scores.json"));
+  },
+  get questionnaireResponseWithUnlinkedAnswers() {
+    // Clone input file contents to avoid one test affecting another
+    return _.cloneDeep(require("./resources/phq9-response-with-unlinked-answers.json"));
+  },
 };
 
 describe("supplements", () => {
@@ -24,6 +32,26 @@ describe("supplements", () => {
           questionnaire: input.questionnaire
         }, r4_model);
       expect(res).toStrictEqual([15]);
+    });
+
+    it("should return correct results when getting some scores from the QuestionnaireResponse resource", () => {
+      const res = fhirpath.evaluate(
+        input.questionnaireResponseWithEmbeddedScores,
+        '%context.repeat(item).where(linkId!=\'total\').answer.weight().sum()',
+        {
+          questionnaire: input.questionnaire
+        }, r4_model);
+      expect(res).toStrictEqual([17]);
+    });
+
+    it("should throw an error when getting scores for answers that doesn't exists in the Questionnaire", () => {
+      const res = () => fhirpath.evaluate(
+        input.questionnaireResponseWithUnlinkedAnswers,
+        '%context.repeat(item).where(linkId!=\'total\').answer.weight().sum()',
+        {
+          questionnaire: input.questionnaire
+        }, r4_model);
+      expect(res).toThrow('Questionnaire answerOptions with these linkIds were not found: /44250-9-unlinked-item,/44259-0.');
     });
   });
 
