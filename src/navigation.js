60e3b8941bd6d8c1afb62afe4b8ba6e8f18e7554
Brian Postlethwaite <brian_pos@hotmail.com>
2025-06-12 16:53:37 +1000

    #170 issues with navigation and extensions - specifically children and descendants

--- src/navigation.js
+++ src/navigation.js
@@ -1,27 +1,36 @@
 const util = require("./utilities");
+const { ResourceNode } = require('./types');
 
 var engine = {};
 
 engine.children = function(coll){
   let model = this.model; // "this" is the context object
   return coll.reduce(function(acc, x){
-    let d = util.valData(x);
-    if (d == null) {
-      return acc;
-    } else if (typeof d === 'object') {
-      for (var prop of Object.keys(d)) {
-        util.pushFn(acc, util.makeChildResNodes(x, prop, model));
-      }
-      return acc;
-    } else {
+    if (!(x instanceof ResourceNode)) {
       return acc;
     }
+    if (typeof x.data === 'object' && x.data != null) {
+      let keys = Object.keys(x.data).filter( k => k !== 'resourceType');
+      for (let prop of keys) {
+        if (!prop.startsWith('_') || !keys.includes(prop.slice(1))) {
+          util.pushFn(acc, util.makeChildResNodes(x, prop, model));
+        }
+      }
+    }
+    else if (typeof x._data === 'object' && x._data != null) {
+      // remove any resourceType keys
+      let keys = Object.keys(x._data).filter( k => k !== 'resourceType');
+      for (let prop of keys) {
+        util.pushFn(acc, util.makeChildResNodes(x, prop, model));
+      }
+    }
+    return acc;
   }, []);
 };
 
 engine.descendants = function(coll){
-  var ch = engine.children.call(this, coll); // "this" is the context object
-  var res = [];
+  let ch = engine.children.call(this, coll); // "this" is the context object
+  let res = [];
   while(ch.length > 0){
     util.pushFn(res, ch);
     ch = engine.children.call(this, ch);
