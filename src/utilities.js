b17b2e9cdfc421d19a2585941b90772a40b484dc
sedinkinya <yury.sedinkin@nih.gov>
2025-06-30 17:41:40 -0400

    Minor changes in comments
    LF-3339

--- src/utilities.js
+++ src/utilities.js
@@ -325,12 +325,17 @@ util.checkAllowAsync = function(ctx, fnName) {
 
 
 /**
- * Reference to the native Object.prototype.hasOwnProperty method, bound to Function.prototype.call.
- * This can be used to safely check if an object has a property as its own (not inherited),
- * avoiding issues if the object has a custom hasOwnProperty property.
+ * Reference to the native Object.prototype.hasOwnProperty method, bound to
+ * Function.prototype.call. This can be used to safely check if an object has
+ * a property as its own (not inherited), avoiding issues if the object has
+ * a custom hasOwnProperty property.
  *
  * Example usage:
- *   util.hasOwnProperty(obj, 'propertyName')
+ * // Cannot use util.hasOwnProperty directly because it triggers the error:
+ * // "Do not access Object.prototype method 'hasOwnProperty' from target object"
+ * const { hasOwnProperty } = require("./utilities");
+ * ...
+ * hasOwnProperty(obj, 'propertyName')
  *
  * @type {Function}
  */
