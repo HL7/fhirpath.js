738d8a084172e921ba05f8469a270ac2770e1663
Brian Postlethwaite <brian_pos@hotmail.com>
2025-08-04 17:34:23 +1000

    Add support for the 6.0.0-ballot3 interim release

---
+++ fhir-context/r6/index.d.ts
@@ -0,0 +1,13 @@
+import {Model} from "../../src/fhirpath";
+
+export const {
+    version,
+    score,
+    choiceTypePaths,
+    pathsDefinedElsewhere,
+    type2Parent,
+    path2Type,
+    resourcesWithUrlParam,
+    path2TypeWithoutElements,
+    path2RefType
+}: Model;
